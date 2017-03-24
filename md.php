31cbdd24036dfcf9cc7bba0b0873f7e59ca30cf8
xiaoyunqiang <xiaoyunqiang@laoyuegou.com>
2017-03-24 10:39:35 +0800

    add md

--- md.php
+++ md.php
@@ -1,3 +1,6 @@
 <?php
-     echo system('/usr/local/bin/markdown_py -o html4 ' . $file . ' > API.html');
+    $source_file = $_GET['file'];
+    $make_file = $_GET['host_root'] . '/doc/API.html';
+    system('/usr/local/bin/markdown_py -o html4 ' . $source_file . ' > ' . $make_file);
+    include $make_file;
 ?>
