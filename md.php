54bbff14b4b9892d19dee86c2db4266e61e08858
xiaoyunqiang <xiaoyunqiang@laoyuegou.com>
2017-03-24 22:14:35 +0800

    fix

--- md.php
+++ md.php
@@ -1,7 +1,6 @@
 <?php
     $source_file = $_GET['file'];
-    $make_file_name = '/doc/' . md5($source_file) . '.html';
-//    $make_file = $_GET['host_root'] . '/doc/API.html';
+    $make_file_name = '/cache/' . 'md_to_html' . '.html';
     $make_file = $_GET['host_root'] . $make_file_name;
     system('/usr/local/bin/markdown_py -o html4 ' . $source_file . ' > ' . $make_file);
     include $make_file;
