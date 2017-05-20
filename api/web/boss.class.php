998970525b38b06333bf2a739f73c2c83d9a3062
xiaoyunqiang <xiaoyunqiang@laoyuegou.com>
2017-05-20 15:53:24 +0800

    add fincance and boos

--- api/web/boss.class.php
+++ api/web/boss.class.php
@@ -24,7 +24,8 @@ class finance extends base {
         $limit = 10;
         $offset = ($page - 1) * $limit;
         $sql_limit = " limit $offset , $limit";
-        $sign = $this->db->getRows("select *  from hqsen_user_kezi_order_sign order by id desc " . $sql_limit);
+        // 总经理要在财务审批通过基础上
+        $sign = $this->db->getRows("select *  from hqsen_user_kezi_order_sign  where sign_status = 1 order by id desc " . $sql_limit);
         foreach ($sign as $one_sign){
             $item['id'] = $one_sign['id'];
             $item['order_money'] = $one_sign['order_money'];
@@ -36,42 +37,25 @@ class finance extends base {
         $this->appDie($this->back_code['sys']['success'], $this->back_msg['sys']['success'], $sign);
     }
 
-    // 客资签单 创建财务审批
+    // 客资签单 创建总经理审批
     public function keziSignFollowCreate(){
         $user_sign_id = $this->postInt('user_sign_id');
         $status_desc = $this->postString('status_desc');
-        $sign_status = $this->postString('sign_status', 1);
+        $sign_status = $this->postString('boss_sign_status', 1);
         // 审批流程数据
-        $sign_follow['sign_status'] = $sign_status;
+        $sign_follow['boss_sign_status'] = $sign_status;
         $sign_follow['status_desc'] = $status_desc;
         $sign_follow['user_sign_id'] = $user_sign_id;
         $sign_follow['create_time'] = time();
         $this->db->insert('hqsen_user_kezi_sign_follow', $sign_follow);
         // 审批成功  更新签单数据  不更新跟踪者订单数据 还是待审核状态
         if(isset($sign_follow['id']) and $sign_follow['id']){
-            $order_sign['sign_status'] = $sign_status;
+            $order_sign['boss_sign_status'] = $sign_status;
             $this->db->update('hqsen_user_kezi_order_sign', $order_sign, ' id = ' . $sign_follow['user_sign_id']);
         }
         $this->appDie();
     }
 
-    // 搭建订单列表
-    public function dajianOrderSignList(){
-        $page = $this->postInt('page', 1);
-        $limit = 10;
-        $offset = ($page - 1) * $limit;
-        $sql_limit = " limit $offset , $limit";
-        $sign = $this->db->getRows("select *  from hqsen_user_dajian_order_sign order by id desc " . $sql_limit);
-        foreach ($sign as $one_sign){
-            $item['id'] = $one_sign['id'];
-            $item['order_money'] = $one_sign['order_money'];
-            $item['order_other_money'] = $one_sign['order_other_money'];
-            $item['sign_pic_count'] = count(json_decode($one_sign['sign_pic']));
-            $item['del_flag'] = $one_sign['del_flag'];//1初次录入 2再次录入 3审批失败
-        }
-        $data['count'] = $this->db->getCount('hqsen_user_dajian_order_sign', 'del_flag != 0');
-        $this->appDie($this->back_code['sys']['success'], $this->back_msg['sys']['success'], $sign);
-    }
 
 
 
