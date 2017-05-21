b0978ebf0e5640a2852617c0aee0912009d26d64
xiaoyunqiang <xiaoyunqiang@laoyuegou.com>
2017-05-21 18:06:48 +0800

    fix

--- api/web/boss.class.php
+++ api/web/boss.class.php
@@ -71,7 +71,7 @@ class boss extends base {
         $item['order_other_money'] = $sign['order_other_money'];
         $item['sign_pic'] = json_decode($sign['sign_pic']);
         $item['sign_using_time'] = date('Y-m-d',$sign['sign_using_time']);
-        $item['sign_status'] = $sign['sign_status'];//1未处理 2通过 3驳回
+        $item['boss_sign_status'] = $sign['boss_sign_status'];//1未处理 2通过 3驳回
 
         $sign_follow_list = $this->db->getRows("select *  from hqsen_user_kezi_sign_follow where user_sign_id = $sign_id order by id desc ");
         $follow_list = [];
@@ -90,6 +90,78 @@ class boss extends base {
         $this->appDie($this->back_code['sys']['success'], $this->back_msg['sys']['success'], $item);
     }
 
+    // 搭建订单列表
+    public function dajianOrderSignList(){
+        $page = $this->postInt('page', 1);
+        $limit = 10;
+        $offset = ($page - 1) * $limit;
+        $sql_limit = " limit $offset , $limit";
+        // 总经理要在财务审批通过基础上
+        $sign = $this->db->getRows("select *  from hqsen_user_dajian_order_sign  where sign_status = 2 and sign_type = 1 order by id desc " . $sql_limit);
+        foreach ($sign as $one_sign){
+            $item['id'] = $one_sign['id'];
+            $item['order_money'] = $one_sign['order_money'];
+            $item['sign_type'] = $one_sign['sign_type'];
+            $item['sign_pic_count'] = count(json_decode($one_sign['sign_pic']));
+            $item['del_flag'] = $one_sign['del_flag'];//0未知 1初次录入 2再次录入
+            $item['boss_sign_status'] = $one_sign['boss_sign_status'];//1未处理 2通过 3驳回
+            $list['list'][] = $item;
+        }
+        $list['count'] = $this->db->getCount('hqsen_user_dajian_order_sign', ' sign_status = 2 and sign_type = 1 ');
+        $this->appDie($this->back_code['sys']['success'], $this->back_msg['sys']['success'], $list);
+    }
 
+    //搭建详情页数据
+    public function dajianSignDetail(){
+        $sign_id = $this->postInt('id');
+        $sign = $this->db->getRow("select *  from hqsen_user_dajian_order_sign where id=" . $sign_id);
+        $item['id'] = $sign['id'];
+        $item['order_money'] = $sign['order_money'];
+        $item['sign_using_time'] = date('Y-m-d',$sign['sign_using_time']);
+        $item['first_order_money'] = $sign['first_order_money'];
+        $item['first_order_using_time'] = date('Y-m-d',$sign['first_order_using_time']);
+        $item['sign_pic'] = json_decode($sign['sign_pic']);
+        $item['boss_sign_status'] = $sign['boss_sign_status'];//1未处理 2通过 3驳回
+
+        $sign_follow_list = $this->db->getRows("select *  from hqsen_user_dajian_sign_follow where user_sign_id = $sign_id order by id desc ");
+        $follow_list = [];
+        foreach ($sign_follow_list as $one_follow){
+            if($one_follow['boss_sign_status'] > 1){
+                $one_item['status_type'] = 1;// 1总经理审核 2 财务审核
+            } else {
+                $one_item['status_type'] = 2;
+            }
+            $one_item['create_time'] = date('Y-m-d',$one_follow['create_time']);
+            $one_item['status'] = $one_follow['boss_sign_status'] > 1 ? $one_follow['boss_sign_status'] : $one_follow['sign_status'];
+            $one_item['status_desc'] = $one_follow['status_desc'];
+            $follow_list[] = $one_item;
+        }
+        $item['follow_list'] = $follow_list;
+        $this->appDie($this->back_code['sys']['success'], $this->back_msg['sys']['success'], $item);
+    }
+
+    // 客资签单 创建总经理审批
+    public function dajianSignFollowCreate()
+    {
+        $user_sign_id = $this->postInt('user_sign_id');
+        $status_desc = $this->postString('status_desc');
+        $sign_status = $this->postString('boss_sign_status', 1);
+        // 审批流程数据
+        if ($user_sign_id and $sign_status) {
+            $sign_follow['boss_sign_status'] = $sign_status;
+            $sign_follow['status_desc'] = $status_desc;
+            $sign_follow['user_sign_id'] = $user_sign_id;
+            $sign_follow['create_time'] = time();
+            $sign_follow['id'] = $this->db->insert('hqsen_user_dajian_sign_follow', $sign_follow);
+            // 审批成功  更新签单数据  不更新跟踪者订单数据 还是待审核状态
+            if (isset($sign_follow['id']) and $sign_follow['id']) {
+                $order_sign['boss_sign_status'] = $sign_status;
+                $this->db->update('hqsen_user_dajian_order_sign', $order_sign, ' id = ' . $sign_follow['user_sign_id']);
+            }
+            $this->appDie();
+        } else {
+            $this->appDie($this->back_code['sys']['value_empty'], $this->back_msg['sys']['value_empty']);
+        }
+    }
 
 }
