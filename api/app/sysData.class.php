c57cbfcd7aac6570dc9a7144cdcbb9fdb2229d77
xiaoyunqiang <xiaoyunqiang@laoyuegou.com>
2017-08-14 15:02:44 +0800

    fix

--- api/app/sysData.class.php
+++ api/app/sysData.class.php
@@ -20,16 +20,17 @@ class sysData extends base {
 
     // 更新数据
     public function updateData(){
-
-        $data['order_type'][] = array('type'=>'1', 'type_name'=>'婚宴');
-        $data['order_type'][] = array('type'=>'2', 'type_name'=>'会务');
-        $data['order_type'][] = array('type'=>'3', 'type_name'=>'生日宴 团宴 宝宝宴');
-
-        $data['order_status'][] = array('type'=>'1', 'type_name'=>'待处理');
-        $data['order_status'][] = array('type'=>'2', 'type_name'=>'跟踪中');
-        $data['order_status'][] = array('type'=>'3', 'type_name'=>'待结算');
-        $data['order_status'][] = array('type'=>'4', 'type_name'=>'已结算');
-        $data['order_status'][] = array('type'=>'5', 'type_name'=>'已取消');
+        $c_appver = $this->postString('appver');
+        $appver = 0;
+//        $data['order_type'][] = array('type'=>'1', 'type_name'=>'婚宴');
+//        $data['order_type'][] = array('type'=>'2', 'type_name'=>'会务');
+//        $data['order_type'][] = array('type'=>'3', 'type_name'=>'生日宴 团宴 宝宝宴');
+//
+//        $data['order_status'][] = array('type'=>'1', 'type_name'=>'待处理');
+//        $data['order_status'][] = array('type'=>'2', 'type_name'=>'跟踪中');
+//        $data['order_status'][] = array('type'=>'3', 'type_name'=>'待结算');
+//        $data['order_status'][] = array('type'=>'4', 'type_name'=>'已结算');
+//        $data['order_status'][] = array('type'=>'5', 'type_name'=>'已取消');
 
 //        $data['order_area'][] = array('area'=>'1', 'area_name'=>'普陀区' , 'area_hotel'=> [
 //            array(
@@ -43,7 +44,23 @@ class sysData extends base {
 //        ]);
 //        $data['order_area'][] = array('area'=>'2', 'area_name'=>'浦东区');
 //        $data['order_area'][] = array('type'=>'3', 'type_name'=>'指定酒店');
-        $data['update_status'] = array('version'=>'1.0', 'update_now'=>'1');// 正常 2强制更新 3普通更新
+        if(isset($c_appver)) {
+            $subs = explode('.', $c_appver);
+            foreach ($subs as $sub) {
+                if ($sub < 10) {
+                    $appver .= (0 . $sub);
+                } else {
+                    $appver .= $sub;
+                }
+            }
+            $appver = intval($appver);
+        }
+        if($appver < 10000){
+            $data['update_status'] = array('version'=>'1.0.0', 'update_now'=>'3');// 正常 2强制更新 3普通更新
+        } else {
+            $data['update_status'] = array('version'=>'1.0.0', 'update_now'=>'1');// 正常 2强制更新 3普通更新
+        }
+
         $data['url'] = 'http://sendevimg.oss-cn-zhangjiakou.aliyuncs.com/upload/package/sen-release.apk';
         $this->appDie($this->back_code['sys']['success'], $this->back_msg['sys']['success'], $data);
     }
